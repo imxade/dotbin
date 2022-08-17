29902b665d915f7a471cb1c8739aadfc6a026a67
imxade <xad3play@gmail.com>
2022-08-18 03:26:30 +0530

    hardened profile fixed

--- .config/osnix/rig/hardened.nix
+++ .config/osnix/rig/hardened.nix
@@ -37,20 +37,6 @@
   };
 
   services = {
-  	tor = {
-		enable = true;
-  	        settings = {
-			SOCKSPort = [
-			   {
-				port = 9090;
-			   }
-			  ];
-#			UseBridges = true;
-#			ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/obfs4proxy";
-#			Bridge = "obfs4 IP:ORPort [fingerprint]";
-  	        };
-  	};
-
   	# enable antivirus clamav and
   	# keep the signatures' database updated
 # 	clamav = {
