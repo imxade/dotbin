393d0963bd4ee8bcc41e510275506b14144c0c62
imxade <xad3play@gmail.com>
2022-08-18 20:36:39 +0530

    updater fixed

--- .config/osnix/update.sh
+++ .config/osnix/update.sh
@@ -2,9 +2,5 @@
 
 pushd "${HOME}/.config/osnix" &&
 nix --experimental-features "nix-command flakes" flake update --commit-lock-file --show-trace &&
-su root -c "
-cp /etc/nixos/hardware-configuration.nix . &&
-nixos-rebuild switch --impure --flake .# --show-trace 
-" &&
 popd || exit
 
