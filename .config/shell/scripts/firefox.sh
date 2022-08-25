5d0fecdabd8f2f17dc3026e3472390240e4b385c
imxade <xad3play@gmail.com>
2022-08-25 21:40:17 +0530

    bluealsa with firefox fixed
    userjs updated

--- .config/shell/scripts/firefox.sh
+++ .config/shell/scripts/firefox.sh
@@ -5,5 +5,5 @@ for i in firefox
 		type "$i" && BROWSER="$i" && break
 	done
 
-firejail --profile='/home/x/.mozilla/firejail/firefox.profile' --whitelist="$HOME/box" --whitelist="$HOME/.asoundrc" --whitelist="/dev/hidraw0" "$BROWSER"
+firejail --profile='/home/x/.mozilla/firejail/firefox.profile' --whitelist="$HOME/box" --whitelist="$HOME/.asoundrc" "$BROWSER"
 
