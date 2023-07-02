454f52f4c6542291b55a639511ccdee24f8b83fc
XADE <xad3play@gmail.com>
2023-07-02 20:24:55 +0530

    clean flatpak before upgrading

--- .config/shell/scripts/backupgrade/non_root.sh
+++ .config/shell/scripts/backupgrade/non_root.sh
@@ -2,5 +2,6 @@
 
 su root -c "sh $HOME/.config/shell/scripts/backupgrade/root.sh" # backup and upgrade distro
 rpm-ostree upgrade                                              # upgrade silverblue
+flatpak uninstall --unused --delete-data -y
 flatpak repair
 flatpak update -y # upgrade flatpak
