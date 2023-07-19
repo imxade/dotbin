2f476272052e684390ff4c5e9aad48eb5239f87e
XADE <xad3play@gmail.com>
2023-07-19 12:50:16 +0530

    fedora upgrade short pastebin -> 0x0

--- .config/shell/scripts/backupgrade/upgrage_immutable_fedora.sh
+++ .config/shell/scripts/backupgrade/upgrage_immutable_fedora.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 STA=$(rpm-ostree status | awk 'NR==3')
-CUR=$(printf "${STA}" | awk -F '/' '{printf $2}')
-ARK=$(printf "${STA}" | awk -F '/' '{printf $3}')
-FLV=$(printf "${STA}" | awk -F '/' '{printf $4}')
-NEW=$(ostree remote refs fedora | awk -v ARK=${ARK} -v FLV=${FLV} '!/raw/ && $0 ~ ARK"/"FLV' | awk -F '/' 'END{print $2}' )
+read -r CUR ARK FLV <<< "$(awk -F '/' '{print $2, $3, $4}' <<< "$STA")"
+NEW=$(ostree remote refs fedora | awk -v ARK="$ARK" -v FLV="$FLV" '!/raw/ && $0 ~ ARK"/"FLV {split($0, a, "/"); ref=a[2]} END{print ref}')
 
-if [ $NEW -gt $CUR ]; then
-  rpm-ostree rebase fedora:fedora/"${NEW}/${ARK}/${FLV}"
+if [[ $NEW -gt $CUR ]]; then
+  rpm-ostree rebase "fedora:fedora/$NEW/$ARK/$FLV"
 else
   rpm-ostree upgrade
 fi
-
