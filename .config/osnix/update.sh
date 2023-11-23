a143c99856169748708af1fc04247243c0da9541
XADE <notadevel@gmail.com>
2023-11-23 17:30:22 +0530

    fixed nix update script

--- .config/osnix/update.sh
+++ .config/osnix/update.sh
@@ -2,6 +2,8 @@
 
 pushd "${HOME}/.config/osnix" &&
 # nix-store --gc &&
+sudo umount -Rl /nix/store
+sudo chown -R ${USER} /nix
 nix-store --delete &&
 nix --experimental-features "nix-command flakes" flake update --commit-lock-file --show-trace &&
 popd || exit
