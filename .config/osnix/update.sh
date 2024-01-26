6571c7ee02044ada274117e775dd7dc7f39dc56b
XADE <notadevel@gmail.com>
2024-01-26 15:23:01 +0530

    wifi working no permssion change when updateing
    cleaned profile.sh

--- .config/osnix/update.sh
+++ .config/osnix/update.sh
@@ -2,9 +2,10 @@
 
 pushd "${HOME}/.config/osnix" &&
 # nix-store --gc &&
-sudo umount -Rl /nix/store
-sudo chown -R ${USER} /nix
-nix-store --delete &&
-nix --experimental-features "nix-command flakes" flake update --commit-lock-file --show-trace &&
+# sudo umount -Rl /nix/store
+# sudo chown -R ${USER} /nix
+# nix-store --delete &&
+sudo nix --experimental-features "nix-command flakes" flake update --commit-lock-file --show-trace &&
+# sudo chown -R root /nix &&
 popd || exit
 
