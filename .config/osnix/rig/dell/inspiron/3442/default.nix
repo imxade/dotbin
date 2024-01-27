6d9946fec0a2026f038040cc5f7742838ec3b502
XADE <notadevel@gmail.com>
2024-01-28 00:21:13 +0530

    cleaned up

--- .config/osnix/rig/dell/inspiron/3442/default.nix
+++ .config/osnix/rig/dell/inspiron/3442/default.nix
@@ -23,25 +23,6 @@
 		exfat
 		ntfs3g
            ];
- 	   etc = {
-#       	# Configure wireplumber   
-#       	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-#       		bluez_monitor.properties = {
-#       			["bluez5.enable-sbc-xq"] = true,
-#       			["bluez5.enable-msbc"] = true,
-#       			["bluez5.enable-hw-volume"] = true,
-#       			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-#       		}
-#       	'';
-		"asound.conf".text = ''
-			pcm_type.bluealsa {
-			  lib ${pkgs.bluez-alsa}/lib/alsa-lib/libasound_module_pcm_bluealsa.so
-			}
-			ctl_type.bluealsa {
-			  lib ${pkgs.bluez-alsa}/lib/alsa-lib/libasound_module_ctl_bluealsa.so
-			}
-		'';
- 	   };
    };
   
    networking = {
@@ -74,22 +55,7 @@
    };
 
    hardware = {
-            enableAllFirmware = true;
-#           pulseaudio = {			# PulseAudio for Audio
-#                   enable = true;
-#       	    support32Bit = true;
-#       	    # Disable Unwanted Modules
-#       	    extraConfig = "
-#       	      unload-module module-suspend-on-idle
-#       	      load-module module-switch-on-connect 
-#       	    ";
-#       	    extraClientConf = "enable-shm=no";
-#       	    daemon = {
-#       		    config = {
-#       			    enable-memfd = "yes"; 
-#       		    };
-#       	    };
-#	    };
+      enableAllFirmware = true;
 	    # Enable Bluetooth
 	    bluetooth = {
 		enable   = true;
@@ -116,58 +82,6 @@
 		];
 	    };
     };
-
-    systemd = {
-            # Profile Specific services
-            services = {
-        	    unblock_wifi = {
-        		enable = true;
-    	    		description = "Unblock Wireless Devices";
-			after = [ 
-			  "wpa_supplicant.service"
-			];
-			before = [
-			  "network.target"
-			];
-			wants = [
-			  "network.target"
-			];
-        		wantedBy = [
-			  "multi-user.target"
-        		];
-   		    	serviceConfig = {
-   		    	    ExecStart = "${pkgs.util-linux}/bin/rfkill unblock wlan";
-        	    	};
-        	    };
-        	    bluealsa = {
-        		enable = false;
-    	    		description = "start Bluetooth for Pure Alsa";
-			after = [ 
-        		  "bluetooth.service" 
-			];
-			before = [
-			  "network.target"
-			];
-			wants = [
-			  "network.target"
-			];
-        		wantedBy = [
-			  "multi-user.target"
-        		];
-   		    	serviceConfig = {
-			    Type = "dbus";
-			    BusName = "org.bluealsa";
-   		    	    ExecStart = "${pkgs.bluez-alsa}/bin/bluealsa";
-#  		    	    ExecStop = "pkill bluealsa";
-#  		    	    Restart = "on-failure";
-        	    	};
-        	    };
-            };
-    };
-
-#   networking = {
-#           enableB43Firmware = true;
-#   };
     
     boot = {
 #	    # Better set the following inside ~/.asoundrc
@@ -183,23 +97,5 @@
  	    extraModulePackages = [
  	      config.boot.kernelPackages.broadcom_sta
  	    ];
-#	    blacklistedKernelModules = [
-#	      "b43" 
-#	      "bcma" 
-#	    ];
-#	    kernelModules = [
-#	      "kvm-intel"
-#	      "nvme"
-#	    ];
-#	    initrd ={
-#	      availableKernelModules = [ 
-#		"ahci" 
-#		"usbhid"
-#	        "xhci_hcd"
-#		"usb_storage" 
-#	      ];
-#	    kernelModules = [
-#	      "nvme"
-#	    ];
     };
  }
