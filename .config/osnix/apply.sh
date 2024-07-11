238a490d814ea9cc2df446414a3b10aaf87d9f55
XADE <notadevel@gmail.com>
2024-07-11 22:06:36 +0530

    fixed: apply to rebuild from different host

--- .config/osnix/apply.sh
+++ .config/osnix/apply.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 pushd "${HOME}/.config/osnix" &&
 sudo su root -c "
-cp /etc/nixos/hardware-configuration.nix . &&
-nixos-rebuild switch --impure --flake .# --show-trace 
+cp /etc/nixos/hardware-configuration.nix . && 
+nixos-rebuild switch --impure --flake .#nixos --show-trace --option sandbox false
+
 " &&
 popd || exit
