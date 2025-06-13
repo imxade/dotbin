1762c372ae820a9c83dd7c69f1ee6ff26d519bf0
XADE <notadevel@gmail.com>
2025-06-13 22:39:43 +0200

    enabled: cosmic-greeter, theme mocha-peach-round (#4)
    Reviewed-on: https://codeberg.org/XADE/dotbin/pulls/4
    Co-authored-by: XADE <notadevel@gmail.com>
    Co-committed-by: XADE <notadevel@gmail.com>

--- .config/osnix/gui/wayland/cosmic/default.nix
+++ .config/osnix/gui/wayland/cosmic/default.nix
@@ -1,16 +1,25 @@
 { lib, pkgs, config, ... }:
 
 {
-  # Include Universal xorg Config
+  # Include Universal wayland Config
   imports = [ ../default.nix ];
 
   # Enable Cosmic Desktop
   services = {
     desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
   };
 
-  environment.cosmic.excludePackages = with pkgs; [
-    cosmic-term
-    cosmic-edit
-  ];
+  environment = {
+    cosmic.excludePackages = with pkgs; [
+      cosmic-term
+      cosmic-edit
+    ];
+    /*
+    systemPackages = with pkgs; [
+      cosmic-ext-tweaks
+    ];
+    */
+  };
 }
+
