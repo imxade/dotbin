7acbb82b5bec55f1c62824b21c9ef24a340d78ab
XADE <notadevel@gmail.com>
2025-06-29 00:24:33 +0530

    flake cleaned

--- .config/osnix/flake.nix
+++ .config/osnix/flake.nix
@@ -5,19 +5,19 @@
   inputs = rec {
 
     # set the channel
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Declaratve flatpak
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
 
     # hardware channel
-    nixos-hardware = { url = "github:nixos/nixos-hardware"; };
+    nixos-hardware.url = "github:nixos/nixos-hardware";
 
     # enable home-manager
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       # tell home manager to use the nixpkgs channel set above.
-      inputs = { nixpkgs = { follows = "nixpkgs"; }; };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
