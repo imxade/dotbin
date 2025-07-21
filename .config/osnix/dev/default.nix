7c077fbb36c4e5adf0ee7de02054bc464284ef9e
XADE <notadevel@gmail.com>
2025-07-21 15:30:04 +0530

    podman to docker

--- .config/osnix/dev/default.nix
+++ .config/osnix/dev/default.nix
@@ -3,22 +3,13 @@
 {
 
   virtualisation = {
-    
+    /*
     podman = {
       enable = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       # dockerCompat = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
-    };
-    waydroid.enable = true;
-    /*
-    docker = {
-      enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
     };
     libvirtd = {
       enable = true;
@@ -28,6 +19,13 @@
       };
     };
     */
+    waydroid.enable = true;
+    docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
   };
 }
-
